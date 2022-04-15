4705667d6b78071b0ac4ac1f35e9cd575ffd13ae
GreedNeSS <ruslan199126@gmail.com>
2022-04-15 20:39:13 +0300

    Added UDP console chat.

--- ConsoleChat/Program.cs
+++ ConsoleChat/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleChat;
+
+Console.WriteLine("***** UDP Client App *****");
+
+Console.WriteLine("Введите порт для получения данных: ");
+int localPort = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите IP адрес: ");
+string address = Console.ReadLine();
+Console.WriteLine("Введите порт для отправки сообщений: ");
+int remotePort = int.Parse(Console.ReadLine());
+
+using (UDPClientApp appClient = new UDPClientApp(localPort, remotePort, address))
+{
+    appClient.ReceiveMessage();
+    Console.WriteLine("Введите сообщение для отправки:");
+
+    while (true)
+    {
+        string message = Console.ReadLine();
+        appClient.SendMessage(message);
+    }
+}
---
+++ ConsoleChat/UDPClientApp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleChat
+{
+    internal class UDPClientApp: IDisposable
+    {
+        private UdpClient sender;
+        private UdpClient receiver;
+        public int LocalPort { get; private set; }
+        public int RemotePort { get; private set; }
+        public string Address { get; private set; }
+
+        public UDPClientApp(int localPort, int remotePort, string ipAddress)
+        {
+            LocalPort = localPort;
+            RemotePort = remotePort;
+            Address = ipAddress;
+            receiver = new UdpClient(localPort);
+            sender = new UdpClient(Address, remotePort);
+        }
+
+        public void SendMessage(string message)
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                sender.Send(buffer, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ReceiveMessage()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    IPEndPoint remoteIp = null;
+
+                    while (true)
+                    {
+                        byte[] buffer = receiver.Receive(ref remoteIp);
+                        string message = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine($"{remoteIp.Address}: {message}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    receiver.Close();
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            if (sender != null)
+            {
+                sender.Close();
+            }
+
+            if (receiver != null)
+            {
+                receiver.Close();
+            }
+        }
+    }
+}
